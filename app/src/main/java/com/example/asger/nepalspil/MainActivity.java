00628014cdfb0bea385e047a5870a2fb770ef712
Nicki <nduris@hotmail.com>
2016-11-22 12:01:06 +0100

    Added field function template + popup class

--- app/src/main/java/com/example/asger/nepalspil/MainActivity.java
+++ app/src/main/java/com/example/asger/nepalspil/MainActivity.java
@@ -1,8 +1,38 @@
 package com.example.asger.nepalspil;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+
 /**
  * Created by Asger on 07-11-2016.
  */
 
-public class MainActivity {
-}
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView im = (ImageView) findViewById(R.id.imageView);
+
+        im.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, imageViewPop.class));
+                if (confirmation().moveToField() == true){
+                    setPlayerPos;
+                    updateTimer;
+                }
+                else
+                    closeWindow;
+            });
+
+        }
+
+    }
---
+++ app/src/main/java/com/example/asger/nepalspil/imageViewPop.java
@@ -0,0 +1,27 @@
+package com.example.asger.nepalspil;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by Nicki on 22-11-2016.
+ */
+
+public class imageViewPop extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text1);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.50),(int) (height*.66));
+
+    }
+}
