2ce66ca8e9d7bb6a34718e88f9ca921f708649c7
Nicki <nduris@hotmail.com>
2016-11-22 13:49:25 +0100

    configured xml board + onclick method for board.

--- app/src/main/java/com/example/asger/nepalspil/Model/spillePlade.java
+++ app/src/main/java/com/example/asger/nepalspil/Model/spillePlade.java
@@ -1,16 +1,36 @@
 package com.example.asger.nepalspil.Model;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.example.asger.nepalspil.R;
 
 /**
  * Created by Nicki on 22-11-2016.
  */
 
-public class spillePlade extends AppCompatActivity implements View.OnClickListener{
+public class spillePlade extends AppCompatActivity {
 
-    @Override
-    public void onClick(View v) {
+    Spiller s = new Spiller("Adam", 50);        //Dummy spiller
+    ImageView im = (ImageView) findViewById(R.id.felt1);
 
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spilplade);
+
+
+        im.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                s.setTid(s.getTid()-1);
+                s.move(1);
+                im.setColorFilter(android.R.color.holo_green_dark);
+                
+            }
+        });
     }
 }
+
