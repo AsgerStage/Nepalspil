c010e309d735319b25ce0ddbe72084fc8ea22647
Nicki <nduris@hotmail.com>
2016-11-22 12:10:36 +0100

    Added field function template + popup class

---
+++ app/src/main/java/com/example/asger/nepalspil/Model/spillePlade.java
@@ -0,0 +1,16 @@
+package com.example.asger.nepalspil.Model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Nicki on 22-11-2016.
+ */
+
+public class spillePlade extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
