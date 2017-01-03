ac15975bd614bf36f8a293ffb0e8d491ead92f5c
Clusive <rdobrandelev@gmail.com>
2017-01-03 13:45:30 +0100

    Added layout files

--- app/src/main/java/com/example/asger/nepalspil/felter/Boghandel.java
+++ app/src/main/java/com/example/asger/nepalspil/felter/Boghandel.java
@@ -9,10 +9,10 @@ import com.example.asger.nepalspil.R;
  * Created by Bruger on 03-01-2017.
  */
 
-public class Lektiehjaelp extends AppCompatActivity {
+public class Boghandel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lektiehjaelp);
+        setContentView(R.layout.boghandel);
 
 
     }
