72482dfccf183d7640d2c3ad33f29d873e4bde4f
Clusive <rdobrandelev@gmail.com>
2017-01-03 13:37:11 +0100

    Added a few classes with core function

---
+++ app/src/main/java/com/example/asger/nepalspil/felter/Lektiehjaelp.java
@@ -0,0 +1,18 @@
+package com.example.asger.nepalspil.felter;
+
+import android.os.Bundle;
+
+import com.example.asger.nepalspil.R;
+
+/**
+ * Created by Bruger on 03-01-2017.
+ */
+
+public class Lektiehjaelp extends Felt {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lektiehjaelp);
+
+
+    }
+}
