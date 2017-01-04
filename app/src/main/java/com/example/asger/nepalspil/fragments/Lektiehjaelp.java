5900431dd3b4a0e7d39116bad3556b8307b63293
Nicki <nduris@hotmail.com>
2017-01-04 15:56:04 +0100

    Added Skole.java to androidmanifest.xml Edited some strings in Skole.java Began field logic for LektieHjaelp

--- app/src/main/java/com/example/asger/nepalspil/fragments/Lektiehjaelp.java
+++ app/src/main/java/com/example/asger/nepalspil/fragments/Lektiehjaelp.java
@@ -1,19 +1,43 @@
 package com.example.asger.nepalspil.fragments;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.asger.nepalspil.R;
+import com.example.asger.nepalspil.models.Spiller;
 
 /**
  * Created by Bruger on 03-01-2017.
  */
 
-public class Lektiehjaelp extends AppCompatActivity {
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.lektiehjaelp);
+public class Lektiehjaelp extends Fragment {
+
+    AlertDialog.Builder dialog;
+    Spiller s;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.lektiehjaelp, container, false);
+    }
 
 
+    public int gemtViden = 0;
+
+    private void lær(){
+        if (gemtViden > 0){
+            s.setViden(s.getViden()+1);
+            s.setTid(s.getTid()-1);
+        }
+        else
+            dialog.setTitle("");
+            dialog.setMessage("Du har ikke brug for lektiehjælp.");
+            dialog.show();
     }
 }
