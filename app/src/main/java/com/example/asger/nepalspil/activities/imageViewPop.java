7957a6edafc126052bb7714c39eecbede178610e
Senad Begovic <Senadbegovic2500@hotmail.com>
2017-01-04 02:55:31 +0100

    Renamed an xml file

--- app/src/main/java/com/example/asger/nepalspil/activities/imageViewPop.java
+++ app/src/main/java/com/example/asger/nepalspil/activities/imageViewPop.java
@@ -15,7 +15,7 @@ public class imageViewPop extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.text1);
+        setContentView(R.layout.marked);
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
