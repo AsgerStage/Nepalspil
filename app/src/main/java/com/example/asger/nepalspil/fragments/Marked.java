e9bec027393556148f715db7ab695287fabe7826
Senad Begovic <Senadbegovic2500@hotmail.com>
2017-01-04 03:16:33 +0100

    Updated marked logic, work button works

--- app/src/main/java/com/example/asger/nepalspil/fragments/Marked.java
+++ app/src/main/java/com/example/asger/nepalspil/fragments/Marked.java
@@ -2,6 +2,9 @@ package com.example.asger.nepalspil.fragments;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import static com.example.asger.nepalspil.activities.MainActivity.spiller;
 
 import com.example.asger.nepalspil.R;
 
@@ -14,6 +17,24 @@ public class Marked extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.marked);
 
+        Button work = (Button) findViewById(R.id.workButton);
+        work.setOnClickListener(new View.OnClickListener() {
 
+            public void onClick(View v) {
+                if (spiller.getTid() >= 2) {
+                    work();
+                }
+                else{
+
+                }
+            }
+        });
     }
+
+
+        public void work(){
+        spiller.setTid(spiller.getTid()-2);
+        spiller.setPenge(spiller.getPenge()+10);
+    }
+
 }
