6a01e92f9ed9886ea573ebdc23c4ce3cdedf0e0e
Nicki <nduris@hotmail.com>
2017-01-04 03:43:17 +0100

    Added field pictures Changed ImageViews to ImageButtons Cleaned build paths.

--- app/src/main/java/com/example/asger/nepalspil/fragments/Skole.java
+++ app/src/main/java/com/example/asger/nepalspil/fragments/Skole.java
@@ -2,8 +2,9 @@ package com.example.asger.nepalspil.fragments;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import com.example.asger.nepalspil.models.Spiller;
 import com.example.asger.nepalspil.R;
+import android.util.Log;
 
 /**
  * Created by Asger on 21-11-2016.
@@ -12,8 +13,35 @@ import com.example.asger.nepalspil.R;
 public class Skole extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lektiehjaelp);              //Har ikke lavet Skole layout endnu
+        setContentView(R.layout.skole);
 
+    }
+
+    Spiller s;
+    private int vidensKrav = 10*s.getKlassetrin();
+
+    public void studer(Spiller s){
+        if (this.harlaert() == true) {
+            s.setViden(s.getViden() + 1);
+            s.setTid(s.getTid() - 1);
+        }
+        else return ;
+
+    }
+
+    public boolean harlaert(){
+        if (Math.random()>0.1)
+            return true;
+        else return false;
+    }
+
+    public void spis(Spiller s){
+        s.setTid(s.getTid()-1);
+        s.setHp(s.getHp()+1);
+    }
+
+    public void startEksamen(Spiller s){
+        if ((s.getViden() !=  vidensKrav ) || (s.getViden() > vidensKrav))
 
     }
 }
