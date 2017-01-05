db34d9f23e569670fe5ac046f761e4274e5c58bf
Nicki <nduris@hotmail.com>
2017-01-05 12:05:00 +0100

    Unfinished logic

--- app/src/main/java/com/example/asger/nepalspil/fragments/Boghandel.java
+++ app/src/main/java/com/example/asger/nepalspil/fragments/Boghandel.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.asger.nepalspil.R;
 
@@ -16,9 +17,13 @@ import static com.example.asger.nepalspil.activities.MainActivity.spiller;
  */
 
 public class Boghandel extends Fragment {
+    Button work, buyBook;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rod = inflater.inflate(R.layout.boghandel, container, false);
 
+      //  work = (Button) rod.findViewById(R.id.bookstoreWork);
+      //  buyBook = (Button) rod.findViewById(R.id.buyBook);
 
 
         return rod;
@@ -30,7 +35,27 @@ public class Boghandel extends Fragment {
     }
 
     private void buyBook() {
-
+        //spiller.setBook(spiller.getBooks()+1)
+        //spiller.setPenge(spiller.getPenge()-10);
     }
 
 }
+//(For help)
+/*
+public class fragmentOne extends Fragment implements OnClickListener {
+    Button myButton;
+
+    @Override
+    public View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedinstanceState) {
+        View myView = inflater.inflate(R.layout.fragment_1, container, false);
+        myButton = (Button) myView.findViewById(R.id.myButton);
+        myButton.setOnClickListener(this);
+        return myView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        // implements your things
+    }
+}
+ */
