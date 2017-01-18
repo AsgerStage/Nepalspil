d4174415cc4f909ea984f4c2b521dc79e58d63c7
psshs <asger.stage@hotmail.com>
2017-01-18 17:21:00 +0100

    Reworked the home-layout and added icons to the top

--- app/src/main/java/com/example/asger/nepalspil/felter/Hjem.java
+++ app/src/main/java/com/example/asger/nepalspil/felter/Hjem.java
@@ -15,17 +15,18 @@ import com.example.asger.nepalspil.activities.SpillePlade;
 
 import static com.example.asger.nepalspil.activities.MainActivity.spiller;
 
-/**
- * Created by Asger on 21-11-2016.
- */
-
 
 public class Hjem extends AppCompatActivity {
+    static TextView textpenge;
+    static TextView textviden;
+    static TextView textmad;
+
     @Override
     public void onBackPressed() {
         SpillePlade.updateEntireBoard();
         finish();
     }
+
     AlertDialog.Builder dialog;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,18 +35,19 @@ public class Hjem extends AppCompatActivity {
 
         ImageView im = (ImageView) findViewById(R.id.hjemprofile);
         dialog = new AlertDialog.Builder(Hjem.this);
-        TextView tv = (TextView) findViewById(R.id.hjemtext);
-        Button bSov = (Button) findViewById(R.id.sleep);
+        textpenge = (TextView) findViewById(R.id.textpenge);
+        textviden = (TextView) findViewById(R.id.textviden);
+        textmad = (TextView) findViewById(R.id.textmad);
+        updateText();
         ImageView hjemBack = (ImageView) findViewById(R.id.hjemBack);
         ImageView hjemhelp = (ImageView) findViewById(R.id.hjemhelp);
 
-        if (spiller.getNavn() == "Asha") {
+        if (spiller.getSex() == false) {
             im.setImageResource(R.drawable.pige2);
         } else {
             im.setImageResource(R.drawable.dreng1);
         }
 
-        tv.setText(("Navn: " + spiller.getNavn() + "\n mad: " + spiller.getHp() + "\n Penge: " + spiller.getPenge() + "\n Viden: " + spiller.getViden() + "\n Klassetrin: " + spiller.getKlassetrin() + "\n Tid: " + spiller.getTid()));
 
         hjemBack.setOnClickListener(new View.OnClickListener() {
 
@@ -61,42 +63,23 @@ public class Hjem extends AppCompatActivity {
             public void onClick(View v) {
                 SpillePlade.updateEntireBoard();
                 v.startAnimation(AnimationUtils.loadAnimation(Hjem.this, R.anim.image_click));
-                dialog.setMessage("Hej jeg hedder "+spiller.getNavn()+ ". Jeg bor med min familie i en landsby i Nepal. \n Min mor og far har aldrig gået i skole, så de tjener ikke så mange penge, så det er svært for dem at hjælpe mig med at få en uddannelse.");
+                dialog.setMessage("Hej jeg hedder " + spiller.getNavn() + ". Jeg bor med min familie i en landsby i Nepal. \n Min mor og far har aldrig gået i skole, så de tjener ikke så mange penge, så det er svært for dem at hjælpe mig med at få en uddannelse.");
                 dialog.show();
             }
         });
 
-        bSov.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                AlertDialog.Builder dialog = new AlertDialog.Builder(Hjem.this);
-                dialog.setTitle("Afslut dagen?");
-                dialog.setMessage("Er du sikker på at du vil afslutte dagen?");
-                dialog.setPositiveButton("Ja", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        spiller.setTid(0);
-                        updateInfo();
-                        SpillePlade.updateTextpenge();
-                        SpillePlade.updateTextmad();
-                        SpillePlade.updateTextviden();
-                        Hjem.this.finish();
-                    }
-                });
-
-                dialog.setNegativeButton("Nej", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        finish();
-                    }
-                });
-                dialog.show();
-
-            }
-        });
     }
 
     public String updateInfo() {
         SpillePlade.updateInfobox();
         return "Navn: " + spiller.getNavn() + "\n mad: " + spiller.getHp() + "\n Penge: " + spiller.getPenge() + "\n Viden: " + spiller.getViden() + "\n Klassetrin: " + spiller.getKlassetrin() + "\n Tid: " + spiller.getTid();
+    }
+
+    public static void updateText() {
+        textpenge.setText(String.valueOf(spiller.getPenge()));
+        textviden.setText(String.valueOf(spiller.getViden()));
+        textmad.setText(String.valueOf(spiller.getHp()));
+
     }
 }
